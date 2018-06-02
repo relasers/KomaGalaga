b86012b1153397494fd3da62168bda4ef8238983
relasers <relasers@gmail.com>
2018-06-02 18:42:52 +0900

    Upload First Time

---
+++ Assets/Script/PlayerScript/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rigidbody2D;
+    public float speed = 150.0f;
+    public float shoot_delay = 0.4f;
+
+    public GameObject PlayerProjectile;
+
+
+    bool CanShoot = true;
+
+    Coroutine coroutine_shooting;
+
+    IEnumerator ShootBulletCoroutine(float delay)
+    {
+
+        GameObject newProjectile = Instantiate(PlayerProjectile,transform.position,Quaternion.identity);
+        ProjectileMover mover = newProjectile.GetComponent<ProjectileMover>();
+        mover.MovingDirection = new Vector2(0,1);
+
+
+
+
+
+        CanShoot = false;
+        yield return new WaitForSeconds(delay);
+        CanShoot = true;
+    }
+
+    void ShootBullet()
+    {
+        if(CanShoot)
+        coroutine_shooting = StartCoroutine(ShootBulletCoroutine(shoot_delay));
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+    private void FixedUpdate()
+    {
+        float velocityX = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+        float velocityY = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
+
+        rigidbody2D.velocity = new Vector2(velocityX , velocityY);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if(0 < Input.GetAxis("Fire1"))
+        ShootBullet();
+
+
+
+    }
+}
---
+++ Assets/Script/ProjectileMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMover : MonoBehaviour {
+
+    public float speed = 150.0f;
+    public Vector2 MovingDirection = new Vector2(0,0);
+    Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+    private void FixedUpdate()
+    {
+
+        rigidbody2D.velocity = MovingDirection * speed * Time.fixedDeltaTime;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("ProjectileRemover"))
+        {
+            Debug.Log("remove");
+            Destroy(gameObject);
+        }
+    }
+
+}
